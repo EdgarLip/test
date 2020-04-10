9c6f96a14d9491fd332fa19752231a6081bc16c4
edgar.lipnitsky <edgar.lipnitsky@imperva.com>
2020-04-10 13:31:43 +0300

    badge_added_5

--- test.groovy
+++ test.groovy
@@ -4,13 +4,9 @@ badge = "nana-banana"
 pipeline {
     agent any
     stages {
-        stage('add_badge') {
-            steps {
-                addBadge(icon: 'completed.gif', text: badge)
-            }
-        }
         stage('check ping to google') {
             steps {
+                addBadge(icon: 'completed.gif', text: badge)
                 sh 'ping 8.8.8.8 -c 5'
             }
         }
